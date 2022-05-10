7b44b5e6d8ccf007e308ce58734746898627376c
connker38 <connker3@googlemail.com>
2022-05-10 19:36:35 +0100

    Hide on scroll

--- Public/app.js
+++ Public/app.js
@@ -1,22 +1,34 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("whole-nav").style.top = "0";
+  } else {
+    document.getElementById("whole-nav").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
+
 function openNav() {
-  document.getElementById("topnav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
-  document.getElementById("topnav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
 
 
 function navBarFunct() {
-  var x = document.getElementById("Mytopnav");
-  if (x.className === "Mysidenav") {
+  var x = document.getElementById("mySidenav");
+  if (x.className === "mySidenav") {
     x.className += " responsive";
   } else {
-    x.className = "Mysidenav";
+    x.className = "mySidenav";
   }
 }
 
@@ -94,3 +106,6 @@ function showError() {
   location.reload();
   return false;
 }
+
+
+
